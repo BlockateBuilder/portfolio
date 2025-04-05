4cc975e242ca3cbcf73dd56d771027c951328a22
IModStuff <116459875+TheGodOfDarkMatter@users.noreply.github.com>
2025-04-05 19:07:08 +0100

    new

---
+++ src/routes/projects/+page.svelte
@@ -0,0 +1,40 @@
+<script>
+</script>
+
+<style>
+  main {
+    max-width: 700px;
+    margin: auto;
+    padding: 4rem 1rem;
+  }
+  h1 {
+    font-size: 3rem;
+    margin-bottom: 0.5rem;
+  }
+  p {
+    font-size: 1.2rem;
+    color: #555;
+  }
+  ul {
+    list-style: none;
+    padding: 0;
+    margin-top: 2rem;
+  }
+  li {
+    margin: 0.5rem 0;
+  }
+  a {
+    text-decoration: none;
+    color: #0070f3;
+  }
+</style>
+
+<main>
+  <h1>Projects</h1>
+  <p>Some projects are in this <a href="https://github.com/BlockateBuilder/Roblox-Projects" target="_blank">github repository</a>, some are also in video form <a href="https://www.youtube.com/@RblxPf-n3t" target="_blank">here</a>.</p>
+  <p>The rest are listed here:</p>
+  <div class="space"></div>
+  <a href="https://create.roblox.com/store/asset/116112656702972" target="_blank">RemoteAnalyser</a>
+  <p>RemoteAnalyserTool (RAT) is a Roblox model which can be required by anyone in-game to show remoteEvents being fired on every client.</p>
+  <img src="" alt='sans'>
+</main>
