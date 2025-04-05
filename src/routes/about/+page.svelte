a887787dd94a12ed1216244731dad9b3b46670a5
IModStuff <116459875+TheGodOfDarkMatter@users.noreply.github.com>
2025-04-05 19:06:54 +0100

    new

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,10 +1,4 @@
 <script>
-  const links = [
-    { name: 'GitHub', href: 'https://github.com/yourname' },
-    { name: 'Twitter', href: 'https://twitter.com/yourhandle' },
-    { name: 'Projects', href: '/projects' },
-    { name: 'Contact', href: 'mailto:you@example.com' }
-  ];
 </script>
 
 <style>
@@ -12,7 +6,6 @@
     max-width: 700px;
     margin: auto;
     padding: 4rem 1rem;
-    font-family: system-ui, sans-serif;
   }
   h1 {
     font-size: 3rem;
@@ -38,9 +31,10 @@
 
 <main>
   <h1>snas</h1>
-  <p>Hey, I'm snas, also known as ._snas on <a href='discord://-/users/814908440496701460'>Discord</a> or as BlockateBuilder01 on <a href='https://www.roblox.com/users/5494278467/profile'>Roblox</a>.</p>
+  <p>Hey, I'm snas, also known as ._snas on <a href='discord://-/users/814908440496701460'>Discord</a> or as BlockateBuilder01 on <a href='https://www.roblox.com/users/5494278467/profile' target="_blank">Roblox</a>.</p>
 
   <ul>
     
   </ul>
+  <img src="https://static.wikia.nocookie.net/noooooooooooo/images/4/41/06A40DEE-E17E-4747-A0DF-B5C73743EB2D.png/revision/latest?cb=20201112015220" alt='sans'>
 </main>
