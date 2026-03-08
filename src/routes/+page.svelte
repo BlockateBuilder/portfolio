af431a1624ba23775ae030adf055e28236d373d0
rhurt <jensont0@icloud.com>
2026-03-08 00:18:37 +0000

    Update +page.svelte
    maybe fix idk

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
-<html lang="en"><head>
+<head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>portfolio</title>
@@ -279,4 +279,4 @@
   </footer>
 
 
-</body></html>
+</body>
